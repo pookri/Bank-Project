5e82c1ca75ac8b5f3956c0ad0bb5839e11a5fa91
pookri <sutharpooja2508@gmail.com>
2021-12-08 22:51:54 -0500

    created new vite project with yarn

--- Bank-UI/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import Start from './src/Start.vue'
-import './src/index.css'
-
-createApp(Start).mount('#app')
--- Bank-UI/src/Start.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-</template>
-
-<script lang="ts" setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-</script>
